62d2570cdddc17d97699126e7486aab3a59fe658
Алихан Эльдерханов <alihanelderhanov@MacBook-Air.local>
2023-02-10 21:42:55 +0300

    fix linter

--- src/shared/lib/classnames/classNames.ts
+++ src/shared/lib/classnames/classNames.ts
@@ -1,15 +1,12 @@
-type Mods = Record<string, boolean | string>;
+type Mods = Record<string, boolean | string>
 
-export function classNames(
-    cls: string,
-    mods: Mods = {},
-    additional: string[] = [],
-): string {
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
     return [
         cls,
         ...additional.filter(Boolean),
         ...Object.entries(mods)
             .filter(([_, value]) => Boolean(value))
             .map(([className]) => className),
-    ].join(' ');
+    ]
+        .join(' ');
 }
