afcd00516e39e4b16b50e8074ff8ebef67ac1256
Алихан Эльдерханов <alihanelderhanov@MacBook-Air.local>
2023-02-10 21:33:59 +0300

    fix linter

--- src/app/providers/App.tsx
+++ src/app/providers/App.tsx
@@ -3,12 +3,11 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
 import { AppRouter } from './router';
 
 const App = () => {
     const { theme } = useTheme();
-
 
     return (
         <div className={classNames('app', {}, [theme])}>
