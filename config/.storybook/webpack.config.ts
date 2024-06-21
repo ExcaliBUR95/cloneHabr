89ae7822cd7e33c3ff3f4eafb724dbed996c24b0
Alikhan <derkhanov96@bk.ru>
2024-06-21 15:58:54 +0300

    fix: правка конфига и вертски

--- config/.storybook/webpack.config.ts
+++ config/.storybook/webpack.config.ts
@@ -1,36 +1,55 @@
-import webpack, { DefinePlugin, RuleSetRule } from 'webpack';
-import path from 'path';
-import { buildCssLoader } from '../build/loaders/buildCssLoader';
-import { BuildPaths } from '../build/types/config';
+import webpack, { DefinePlugin, RuleSetRule } from "webpack";
+import path from "path";
+import { buildCssLoader } from "../build/loaders/buildCssLoader";
+import { BuildPaths } from "../build/types/config";
 
-export default ({ config }: {config: webpack.Configuration}) => {
-    const paths: BuildPaths = {
-        build: '',
-        html: '',
-        entry: '',
-        src: path.resolve(__dirname, '..', '..', 'src'),
+export default ({ config }: { config: webpack.Configuration }) => {
+  const paths: BuildPaths = {
+    build: "",
+    html: "",
+    entry: "",
+    src: path.resolve(__dirname, "..", "..", "src"),
+  };
+
+  // Проверяем и модифицируем config.resolve.modules
+  if (config.resolve?.modules) {
+    config.resolve.modules.push(paths.src);
+  } else {
+    config.resolve = {
+      ...config.resolve,
+      modules: [paths.src],
     };
-    config.resolve?.modules?.push(paths.src);
-    config.resolve?.extensions?.push('.ts', '.tsx');
+  }
 
-    // eslint-disable-next-line no-param-reassign
-    config?.module?.rules = config?.module?.rules?.map((rule: RuleSetRule) => {
-        if (/svg/.test(rule.test as string)) {
-            return { ...rule, exclude: /\.svg$/i };
-        }
+  // Проверяем и модифицируем config.resolve.extensions
+  config.resolve = config.resolve ?? {};
+  config.resolve.extensions = config.resolve.extensions ?? [];
+  config.resolve.extensions.push(".ts", ".tsx");
 
-        return rule;
-    });
+  // Обработка правил для модулей
+  config.module = config.module ?? {};
+  config.module.rules =
+    config.module.rules?.map((rule: RuleSetRule | any) => {
+      if (/svg/.test(rule.test as string)) {
+        return { ...rule, exclude: /\.svg$/i };
+      }
+      return rule;
+    }) ?? [];
 
-    config?.module?.rules?.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-    });
-    config?.module?.rules?.push(buildCssLoader(true));
+  // Добавляем правила для SVG и CSS
+  config.module.rules.push({
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  });
+  config.module.rules.push(buildCssLoader(true));
 
-    config.plugins?.push(new DefinePlugin({
-        __IS_DEV__: true
-    }));
+  // Добавление плагинов
+  config.plugins = config.plugins ?? [];
+  config.plugins.push(
+    new DefinePlugin({
+      __IS_DEV__: true,
+    })
+  );
 
-    return config;
+  return config;
 };
